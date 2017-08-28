c9f97f7d4b9e47881d5fb8d0d8d3863c5abb5ea5
Ravy <smart.fz@mail.ru>
2017-08-28 18:51:32 +0300

    #1

--- assets/rovniy/modules/header/header.controller.js
+++ assets/rovniy/modules/header/header.controller.js
@@ -4,15 +4,40 @@
     angular.module('rovniy')
         .controller('headerController', headerController);
 
-    headerController.$inject = [];
+    headerController.$inject = ['$scope'];
 
-    function headerController() {
+    function headerController($scope) {
         var vm = this;
+        vm.active = false;
 
         activate();
         /////////////////////
         function activate() {
+            timer();
+        }
 
+        /**
+         * Часы и время в футере
+         */
+        function timer(){
+            var date = new Date();
+            var hours = date.getUTCHours();
+            var minutes = date.getUTCMinutes();
+            var seconds = date.getUTCSeconds();
+            var day = date.getUTCDay();
+            var mounth = date.getUTCMonth();
+            var year = date.getUTCDate();
+            if (hours < 10) hours = "0" + hours;
+            if (minutes < 10) minutes = "0" + minutes;
+            if (seconds < 10) seconds = "0" + seconds;
+            if (day < 10) day = "0" + day;
+            if (mounth < 10) mounth = "0" + mounth;
+            $scope.currentTime = hours + ":" + minutes + ":" + seconds;
+            $scope.currentDate = day + "." + mounth + "." + year;
+            setTimeout(function(){
+                timer();
+                $scope.$digest();
+            }, 1000);
         }
 
     }
